1eec92528bd5f9a87a06e543f322ee7d87e11b8e
Chris Calvani <calvanic2001@gmail.com>
2024-07-16 16:45:07 -0400

    Files created & used during course

---
+++ Skills Assessment/flag.s
@@ -0,0 +1,37 @@
+global _start
+
+section .text
+_start:
+    ; push './flg.txt\x00'
+    push rax            ; push NULL string terminator
+    mov rdi, '/flg.txt' ; rest of file name
+    push rdi            ; push to stack 
+    
+    ; open('rsp', 'O_RDONLY')
+    mov al, 2          ; open syscall number
+    mov rdi, rsp        ; move pointer to filename
+    xor rsi, rsi        ; set O_RDONLY flag
+    syscall
+
+    ; read file
+    lea rsi, [rdi]      ; pointer to opened file
+    mov rdi, rax        ; set fd to rax from open syscall
+    xor rax, rax        ; read syscall number
+    mov dl, 24         ; size to read
+    syscall
+
+    ; write output
+    xor rax, rax
+    xor rdi, rdi
+    mov al, 1          ; write syscall
+    mov dil, 1          ; set fd to stdout
+    mov dl, 24         ; size to read
+    syscall
+
+    ; exit
+    xor rax, rax
+    mov al, 60
+    xor rdi, rdi
+    syscall
+
+
